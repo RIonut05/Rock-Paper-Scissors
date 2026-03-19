57e2d8bf5887610a1638c905f436c9b2886c1367
ionut <rautuionut7@gmail.com>
2026-03-19 10:59:20 +0200

    finished the main logic of the game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rock-paper-scissors
+# ✊✋✌️ Rock Paper Scissors Game
+
+A simple and interactive Rock Paper Scissors game built using HTML, CSS, and JavaScript. Play against the computer and test your luck!
---
+++ main.js
@@ -0,0 +1,38 @@
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors']
+
+  return choices[Math.floor(Math.random() * 3)]
+}
+
+function getHumanChoice() {
+  return prompt('Choose your wepon').toLowerCase()
+}
+
+function playGame() {
+  let humanScore = 0
+  let computerScore = 0
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log('Draw!')
+    } else if (
+      humanChoice === 'rock' && computerChoice === 'scissors' ||
+      humanChoice === 'paper' && computerChoice === 'rock' ||
+      humanChoice === 'scissors' && computerChoice === 'rock'
+    ) {
+      console.log('You win!')
+      ++humanScore
+    } else {
+      console.log('You lose!')
+      ++computerScore
+    }
+
+    console.log(`human: ${humanScore} - computer: ${computerScore}`)
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice())
+  }
+}
+
+playGame()
